9baf76ec41a28475e48df61ca85153b555be3398
gabrielearaujo <gabimitusa@gmail.com>
2024-04-15 21:14:27 -0300

    update

--- Atividade 1/Atividade_1/src/Main.java
+++ Atividade 1/Atividade_1/src/Main.java
@@ -1,3 +1,4 @@
+import java.util.Calendar;
 import java.util.Scanner;
 
 public class Main {
@@ -8,7 +9,10 @@ public class Main {
         System.out.println("Escolha uma dessas operações:\n1 - Calcular Média ponderada\n2 - Desconto no Preço de Produto" +
                 "\n3 - Calcular Salário Final\n4 - Calcular Novo Peso\n5 - Calcular Área do Trapézio" +
                 "\n6 - Calcular Área do Losango\n7 - Calcular Idade\n8 - Calcular Esfera\n9 - Calcular Diagonais do Polígono" +
-                "\n10 - Converter Moedas");
+                "\n10 - Converter Moedas\n11 - Calcular Produto de Dois Números\n12 - Calcular Média de Três Números" +
+                "\n13 - Calcular Raízes de Equação do 2º Grau\n14 - Calcular Diferença entre Dois Números" +
+                "\n15 - Determinar Maior e Menor Número\n16 - Calcular distância entre Pontos P(X1,Y1) e Q(X2,Y2)\n17 - Calcular IMC" +
+                "\n18 - Idade para Votar e tirar CNH\n19 - Calcular pagamento de produto\n20 - Mostrar Tabuada");
 
         System.out.println("Digite uma opção: ");
 
@@ -43,6 +47,36 @@ public class Main {
                 break;
             case 10:
                 converterMoedas();
+                break;
+            case 11:
+                calcularProduto();
+                break;
+            case 12:
+                calcularMediaTresNumeros();
+                break;
+            case 13:
+                calcularEquacaoSegundoGrau();
+                break;
+            case 14:
+                calcularDiferenca();
+                break;
+            case 15:
+                determinarMaiorMenor();
+                break;
+            case 16:
+                calcularDistanciaEntrePontos();
+                break;
+            case 17:
+                calcularIMC();
+                break;
+            case 18:
+                verificarIdadeParaVotarECNH();
+                break;
+            case 19:
+                calcularValorAPagar();
+                break;
+            case 20:
+                mostrarTabuada();
                 break;
             default:
                 System.out.println("Opção inválida!");
@@ -172,4 +206,197 @@ public class Main {
         System.out.println("Valor em euros: " + euros + " €");
         System.out.println("Valor em libras esterlinas: £" + libras);
     }
+
+    // Exercício 11
+    public static void calcularProduto() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Digite o primeiro número: ");
+        double num1 = sc.nextDouble();
+        System.out.println("Digite o segundo número: ");
+        double num2 = sc.nextDouble();
+        double produto = num1 * num2;
+        System.out.println("O produto dos números é: " + produto);
+    }
+
+    // Exercício 12
+    public static void calcularMediaTresNumeros() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Digite o primeiro número: ");
+        double num1 = sc.nextDouble();
+        System.out.println("Digite o segundo número: ");
+        double num2 = sc.nextDouble();
+        System.out.println("Digite o terceiro número: ");
+        double num3 = sc.nextDouble();
+        double media = (num1 + num2 + num3) / 3;
+        System.out.println("A média dos números é: " + media);
+    }
+
+    // Exercício 13
+    public static void calcularEquacaoSegundoGrau() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Digite o valor de a: ");
+        double a = sc.nextDouble();
+        System.out.println("Digite o valor de b: ");
+        double b = sc.nextDouble();
+        System.out.println("Digite o valor de c: ");
+        double c = sc.nextDouble();
+        double delta = b * b - 4 * a * c;
+        if (delta >= 0) {
+            double raiz1 = (-b + Math.sqrt(delta)) / (2 * a);
+            double raiz2 = (-b - Math.sqrt(delta)) / (2 * a);
+            System.out.println("As raízes da equação são: " + raiz1 + " e " + raiz2);
+        } else {
+            System.out.println("Não é possível calcular as raízes reais para esta equação.");
+        }
+    }
+
+    // Exercício 14
+    public static void calcularDiferenca() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Digite o primeiro número: ");
+        double num1 = sc.nextDouble();
+        System.out.println("Digite o segundo número: ");
+        double num2 = sc.nextDouble();
+        double diferenca;
+        if (num1 > num2) {
+            diferenca = num1 - num2;
+        } else {
+            diferenca = num2 - num1;
+        }
+        System.out.println("A diferença entre os números é: " + diferenca);
+    }
+
+    // Exercício 15
+    public static void determinarMaiorMenor() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Digite o primeiro número: ");
+        int num1 = sc.nextInt();
+        System.out.println("Digite o segundo número: ");
+        int num2 = sc.nextInt();
+        System.out.println("Digite o terceiro número: ");
+        int num3 = sc.nextInt();
+        int maior = Math.max(num1, Math.max(num2, num3));
+        int menor = Math.min(num1, Math.min(num2, num3));
+        System.out.println("O maior número é: " + maior);
+        System.out.println("O menor número é: " + menor);
+    }
+
+
+    // Exercício 16 - Fórmula= dAB=raiz((x_2-x_1)^2+(y_2-y_1)^2)
+    public static void calcularDistanciaEntrePontos() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Digite as coordenadas do ponto P (X1,Y1): ");
+        double x1 = sc.nextDouble();
+        double y1 = sc.nextDouble();
+        System.out.println("Digite as coordenadas do ponto Q (X2,Y2): ");
+        double x2 = sc.nextDouble();
+        double y2 = sc.nextDouble();
+
+        double distancia = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+        System.out.println("A distância entre os pontos P e Q é: " + distancia);
+    }
+
+    // Exercício 17
+    public static void calcularIMC() {
+        Scanner sc = new Scanner(System.in);
+        // letra a - IMC
+        System.out.println("Digite o peso (em kg): ");
+        double peso = sc.nextDouble();
+        System.out.println("Digite a altura (em metros): ");
+        double altura = sc.nextDouble();
+
+        double imc = peso / (altura * altura);
+        System.out.println("Seu IMC é: " + imc);
+
+        // letra b - Classificação do IMC
+        if (imc < 19.99) {
+            System.out.println("Classificação: Magreza");
+        } else if (imc >= 20 && imc <= 24.99) {
+            System.out.println("Classificação: Normal");
+        } else if (imc >= 25 && imc <= 29.99) {
+            System.out.println("Classificação: Excesso de Peso");
+        } else if (imc >= 30 && imc <= 35) {
+            System.out.println("Classificação: Obesidade");
+        } else {
+            System.out.println("Classificação: Grande Obesidade");
+        }
+    }
+
+    // Exercício 18
+    public static void verificarIdadeParaVotarECNH() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Digite o ano de nascimento: ");
+        int anoNascimento = sc.nextInt();
+
+        // Calculando a idade
+        int anoAtual = Calendar.getInstance().get(Calendar.YEAR);
+        int idade = anoAtual - anoNascimento;
+
+        System.out.println("Sua idade é: " + idade + " anos.");
+
+        // Verificando se a pessoa tem idade para votar e obter CNH
+        if (idade >= 16) {
+            System.out.println("Você tem idade para votar.");
+        } else {
+            System.out.println("Você ainda não tem idade para votar.");
+        }
+
+        if (idade >= 18) {
+            System.out.println("Você tem idade para obter a Carteira de Habilitação (CNH).");
+        } else {
+            System.out.println("Você ainda não tem idade para obter a Carteira de Habilitação (CNH).");
+        }}
+
+
+    // Exercício 19
+    public static void calcularValorAPagar() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Digite o preço normal de etiqueta do produto: ");
+        double precoEtiqueta = sc.nextDouble();
+
+        System.out.println("Escolha a condição de pagamento:");
+        System.out.println("1 - À vista em dinheiro ou cheque (recebe 20% de desconto)");
+        System.out.println("2 - À vista no cartão de crédito (recebe 10% de desconto)");
+        System.out.println("3 - Em duas vezes (preço normal da etiqueta sem juros)");
+        System.out.println("4 - Em três vezes (preço normal de etiqueta mais juros de 5%)");
+        int escolha = sc.nextInt();
+
+        double valorAPagar;
+
+        switch (escolha) {
+            case 1:
+                valorAPagar = precoEtiqueta * 0.8; // Desconto de 20%
+                break;
+            case 2:
+                valorAPagar = precoEtiqueta * 0.9; // Desconto de 10%
+                break;
+            case 3:
+                valorAPagar = precoEtiqueta / 2; // Preço normal em duas vezes sem juros
+                System.out.println("O valor parcelado é: 2x de R$" + valorAPagar);
+                break;
+            case 4:
+                valorAPagar = (precoEtiqueta / 3 ) * 1.05; // Preço normal mais juros de 5%
+                System.out.println("O valor parcelado é: 3x de R$" + valorAPagar + " incluindo 5% de juros");
+                break;
+            default:
+                System.out.println("Opção inválida!");
+                return;
+        }
+
+        System.out.println("O valor a ser pago é: R$" + valorAPagar);
+    }
+
+    // Exercício 20
+    public static void mostrarTabuada() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("De qual número você quer a Tabuada? ");
+        int num = sc.nextInt();
+
+        System.out.println("Tabuada do " + num + ":");
+        for (int i = 0; i <= 10; i++) {
+            System.out.println(num + " x " + i + " = " + (num * i));
+        }
+    }
+
 }
+
