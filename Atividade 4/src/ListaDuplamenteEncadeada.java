44c15f00750b4e0c995f761603e34ec9089df13a
gabrielearaujo <gabimitusa@gmail.com>
2024-06-13 12:00:37 -0300

    update

--- Atividade 4/src/ListaDuplamenteEncadeada.java
+++ Atividade 4/src/ListaDuplamenteEncadeada.java
@@ -13,46 +13,46 @@ public class ListaDuplamenteEncadeada {
 
     // Adicionar um novo nodo ao final da lista
     public void adicionar(int dado) {
-        Nodo novoNodo = new Nodo(dado);
+        Nodo newNodo = new Nodo(dado);
         if (cabeca == null) {  // verificar se a lista tá vazia
-            cabeca = novoNodo;  // novo nodo como a cabeça da lista
+            cabeca = newNodo;  // novo nodo como a cabeça da lista
         } else {
-            Nodo atual = cabeca;  // buscar pelo final da lista a partir da cabeça
-            while (atual.proximo != null) {  // percorrer a lista até encontrar o último nodo
-                atual = atual.proximo;
+            Nodo nodoAtual = cabeca;  // buscar pelo final da lista a partir da cabeça
+            while (nodoAtual.proximo != null) {  // percorrer a lista até encontrar o último nodo
+                nodoAtual = nodoAtual.proximo;
             }
-            atual.proximo = novoNodo;  // adicionar o novo nodo ao final da lista
+            nodoAtual.proximo = newNodo;  // adicionar o novo nodo ao final da lista
         }
     }
 
     // Imprimir todos os nodos da lista
     public void imprimirLista() {
-        Nodo atual = cabeca;
-        while (atual != null) {  // percorrer a lista até o final
-            System.out.print(atual.dado + " ");
-            atual = atual.proximo;
+        Nodo nodoAtual = cabeca;
+        while (nodoAtual != null) {  // percorrer a lista até o final
+            System.out.print(nodoAtual.dado + " ");
+            nodoAtual = nodoAtual.proximo;
         }
         System.out.println();
     }
 
     // Analisar a lista
     public void analisarLista(int valor) {
-        Nodo atual = cabeca;
+        Nodo nodoAtual = cabeca;
         int totalNodos = 0;
         int nodosIguais = 0;
         int nodosMaiores = 0;
         List<Integer> posicoesIguais = new ArrayList<>();  // guardar posições de nodos com valor igual
 
         int posicao = 1;  //
-        while (atual != null) {
+        while (nodoAtual != null) {
             totalNodos++;
-            if (atual.dado == valor) {  // verificar se o valor do nodo é igual ao fornecido
+            if (nodoAtual.dado == valor) {  // verificar se o valor do nodo é igual ao fornecido
                 nodosIguais++;
                 posicoesIguais.add(posicao);
-            } else if (atual.dado > valor) {  // verifica se o valor do nodo é maior que o passado
+            } else if (nodoAtual.dado > valor) {  // verifica se o valor do nodo é maior que o passado
                 nodosMaiores++;
             }
-            atual = atual.proximo;
+            nodoAtual = nodoAtual.proximo;
             posicao++;
         }
         System.out.println("Total de nodos: " + totalNodos);
